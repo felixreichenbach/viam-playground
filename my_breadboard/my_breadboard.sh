19fb8af3b4ce5041a26155489b2732d8ccda8487
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-07-03 17:41:15 +0200

    my_breadboard removed

--- my_breadboard/my_breadboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-export PYTHONPATH `/home/felixreichenbach/my_breadboard`
-echo $PYTHONPATH
-
-# Be sure to use `exec` so that termination signals reach the python process,
-# or handle forwarding termination signals manually
-exec python3 -m my_breadboard.main $@
