9f8e59e229c3211db3cacc9b50f58eb28512dd0d
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2023-07-11 16:11:41 +0200

    added

---
+++ viam_modules/foo/src/main.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from viam.module.module import Module
+from viam.components.arm import Arm
+
+from ..src import Foo, MyFoo
+
+
+async def main():
+    """This function creates and starts a new module, after adding all desired resources.
+    Resources must be pre-registered. For an example, see the `src.__init__.py` file.
+    """
+
+    module = Module.from_args()
+    module.add_model_from_registry(Foo.SUBTYPE, MyFoo.MODEL)
+    await module.start()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
